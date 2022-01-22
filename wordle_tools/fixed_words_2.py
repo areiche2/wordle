bab26bf04c7061962add65e8ad05a98d07cac224
Adam R <areiche2@users.noreply.github.com>
2022-01-21 23:03:48 -0800

    Adding prime list. Adding additional stats to program for testing 4 words.

--- wordle_tools/fixed_words_2.py
+++ wordle_tools/fixed_words_2.py
@@ -4,21 +4,22 @@ import json
 import lexicon
 import matches
 
-# all_matches = {
-#    (t, g): matches.matches(target=t, guess=g)
-#    for t, g in itertools.product(lexicon.lexicon, repeat=2)
-# }
-
 words = ["brake", "dying", "clots", "whump"]
 combos = collections.defaultdict(list)
-for w in lexicon.daily_words:
+all_words = lexicon.lexicon
+for w in all_words:
     c = tuple(matches.matches(target=w, guess=g) for g in words)
     combos[c].append(w)
 print(
     json.dumps(
         {
-            "-".join(resp): {"words": sorted(ws), "len": len(ws)}
-            for resp, ws in combos.items()
+            "words": words,
+            "score_n": sum(1 if len(ws) == 1 else 2 for ws in combos.values()),
+            "score_d": len(all_words),
+            "signatures": {
+                "-".join(resp): {"words": sorted(ws), "len": len(ws)}
+                for resp, ws in combos.items()
+            },
         },
         sort_keys=True,
         indent=4,
