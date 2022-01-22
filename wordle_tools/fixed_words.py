72853751ca0ffb0948a7d5290afbcc9af317d5b2
Adam R <areiche2@users.noreply.github.com>
2022-01-22 10:28:15 -0800

    Added prime file and split testing

--- wordle_tools/fixed_words.py
+++ wordle_tools/fixed_words.py
@@ -1,25 +1,51 @@
-import random
+import collections
+import itertools
+import json
 
 import lexicon
 import matches
 
-# all_matches = {
-#    (t, g): matches.matches(target=t, guess=g)
-#    for t, g in itertools.product(lexicon.lexicon, repeat=2)
-# }
 
-for i in range(10000000):
-    if i % 10000 == 0:
-        print(f"Round {i}")
-    words = ["tares", "cough", "blimp", "windy"]
-    words.extend(random.sample(lexicon.lexicon, 3))
-    combos = {}
+def get_combos(p):
+    combos = collections.defaultdict(list)
     for w in lexicon.daily_words:
-        c = tuple(matches.matches(target=w, guess=g) for g in words)
-        if c in combos:
-            break
-        combos[c] = w
-    else:
-        print("Words: ", words)
-        print("All Unique")
-        exit()
+        c = tuple(matches.matches(target=w, guess=g) for g in p)
+        combos[c].append(w)
+    return combos
+
+
+def score(p):
+    combos = get_combos(p)
+    return sum(1 if len(b) == 1 else 2 for b in combos.values())
+
+
+def get_scores():
+    p = ("brake", "dying", "clots", "whump")
+    res = {}
+    res[p] = score(p)
+    for subset in itertools.combinations(p, len(p) - 1):
+        for w in lexicon.lexicon:
+            if w not in p:
+                neighbor = subset + (w,)
+                res[neighbor] = score(neighbor)
+    return res
+
+
+if __name__ == "__main__":
+    res = get_scores()
+    best = max(res.items(), key=lambda x: x[1])
+    print(
+        json.dumps(
+            {
+                "words": best[0],
+                "score_n": best[1],
+                "score_d": len(lexicon.daily_words),
+                "signatures": {
+                    "-".join(resp): sorted(ws)
+                    for resp, ws in get_combos(best[0]).items()
+                },
+            },
+            sort_keys=True,
+            indent=4,
+        )
+    )
--- wordle_tools/fixed_words_2.py
+++ wordle_tools/fixed_words_2.py
@@ -6,7 +6,7 @@ import matches
 
 words = ["brake", "dying", "clots", "whump"]
 combos = collections.defaultdict(list)
-all_words = lexicon.lexicon
+all_words = lexicon.daily_words
 for w in all_words:
     c = tuple(matches.matches(target=w, guess=g) for g in words)
     combos[c].append(w)
@@ -16,10 +16,7 @@ print(
             "words": words,
             "score_n": sum(1 if len(ws) == 1 else 2 for ws in combos.values()),
             "score_d": len(all_words),
-            "signatures": {
-                "-".join(resp): {"words": sorted(ws), "len": len(ws)}
-                for resp, ws in combos.items()
-            },
+            "signatures": {"-".join(resp): sorted(ws) for resp, ws in combos.items()},
         },
         sort_keys=True,
         indent=4,
