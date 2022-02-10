028f3e625a6f25cf5d0dcaf7440cc25a1b3135c3
Adam R <areiche2@users.noreply.github.com>
2022-02-09 22:02:05 -0800

    fix spacing

--- scraper/wordle_daily.py
+++ scraper/wordle_daily.py
@@ -1,5 +1,7 @@
 from datetime import date, timedelta
+
 from words import starting_words
-orig = date(2021,6,19)
+
+orig = date(2021, 6, 19)
 for i in range(len(starting_words)):
-    print(i, orig+timedelta(days=i), starting_words[i], sep="\t")
+    print(i, orig + timedelta(days=i), starting_words[i], sep="\t")
